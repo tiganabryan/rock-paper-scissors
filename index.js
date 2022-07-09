31525ea2bd69be1152fe21d093a20c16e1c13890
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-07-09 14:27:16 -0400

    Add score and winner to the dom

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ let playerSelection = ''
 let computerScore = 0
 let playerScore = 0
 
+let scoreBoard = document.querySelector('.score-board')
+let playerScoreText = document.querySelector('.player-score')
+let computerScoreText = document.querySelector('.computer-score')
+
+// playerScoreText.textContent = `your score: ${playerScore}`
+// computerScoreText.textContent = `computer's score: ${computerScore}`
+
+    
+
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   };
@@ -18,36 +27,50 @@ computerPlay = () => {
 }
 
 
-playRound = (computerSelection) => {
+const playRound = (computerSelection) => {
     playerSelection = prompt('rock, paper, or scissors? ')
-    playerSelection = playerSelection.toLowerCase()
+    playerSelection.toLowerCase()
     computerSelection = computerPlay()
 
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'paper') ||
     (playerSelection == 'paper' && computerSelection == 'rock')) {
-        playerScore += 1
+        return playerScore += 1
 
     } else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
     (computerSelection == 'scissors' && playerSelection == 'paper') ||
     (computerSelection == 'paper' && playerSelection == 'rock')) {
-        computerScore += 1
+        return computerScore += 1
 
     } else if (computerSelection == playerSelection) {
         log('tie')
 
     } else {
-        return `please try again! ${playerSelection} is not a valid answer`
+        log('error')
     }
 
-    return [playerScore, computerScore]
+     
+
+    // debugger
+    // scoreBoard.appendChild(playerScoreText)
+    // scoreBoard.appendChild(computerScoreText)
+    // debugger
+
+    // return playerScore, computerScore
 }
 
 
-game = () => {
+const game = () => {
     for (let i = 0; i < 5; i++) {
         log(playRound())
+        playerScoreText.textContent = `your score: ${playerScore}`
+        computerScoreText.textContent = `computer's score: ${computerScore}`
+        // return playerScore, computerScore
     }
+    const winner = document.querySelector('.winner')
+    winner.textContent = playerScore > computerScore ? 'you won this game!' : 'computer won this game!'
+
+    // scoreBoard.appendChild(winner)
 }
 
 game()
